9578d68afbb3ef877f0304cebb6c57941d4f5f28
dhnbroken <bdfnatsu@gmail.com>
2024-11-25 10:58:57 +0700

    fix: fix some bugs

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,22 @@
-import { draftMode } from "next/headers";
-import MoreStories from "./more-stories";
+import { draftMode } from 'next/headers';
+import MoreStories from './more-stories';
 
-import { getAllMenuItems, getAllPosts } from "@/lib/api";
-import { CMS_NAME, CMS_URL } from "@/lib/constants";
+import { getAllMenuItems, getAllPosts } from '@/lib/api';
+import { CMS_NAME, CMS_URL } from '@/lib/constants';
 
-import HeroPost from "./components/HeroPost";
+import HeroPost from './components/HeroPost';
 
 function Intro() {
   return (
     <section className="Intro flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
-      </h1>
+      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">Blog.</h1>
       <h2 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{" "}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-success duration-200 transition-colors"
-        >
+        A statically generated blog example using{' '}
+        <a href="https://nextjs.org/" className="underline hover:text-success duration-200 transition-colors">
           Next.js
-        </a>{" "}
-        and{" "}
-        <a
-          href={CMS_URL}
-          className="underline hover:text-success duration-200 transition-colors"
-        >
+        </a>{' '}
+        and{' '}
+        <a href={CMS_URL} className="underline hover:text-success duration-200 transition-colors">
           {CMS_NAME}
         </a>
         .
@@ -34,14 +26,14 @@ function Intro() {
 }
 
 export default async function Page() {
-  const { isEnabled } = draftMode();
+  const { isEnabled } = await draftMode();
   const allPosts = await getAllPosts(isEnabled);
   const heroPost = allPosts[0];
   const morePosts = allPosts.slice(0);
 
   const allMenuItems = await getAllMenuItems();
   const allItems = allMenuItems;
-  console.log(allItems)
+  console.log(allItems);
 
   return (
     <div className="Page container mx-auto px-5">
